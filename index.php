71de266890de6a96e6d01f48a24efe24f4609e83
abhivero <36040521+abhivero@users.noreply.github.com>
2018-08-07 13:30:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h1>Check that out!</h1>";
 echo "<p>You've just created your first Web App for Containers app!</p>";
-echo "<p>!!! This is really exciting !!!</p>";
+echo "<p>!!! I think that this is really exciting !!!</p>";
 ?>
